6f11338ed6236838c89bfe87d1d8cc6fd02b9c68
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-08 07:35:39 +0100

    add optimized_bubble_sort function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-fn main() {
-        let mut arr = vec![8, 1, -8, 5, 0, 7, -2, 2, -6, -1, 3, -3, -4, -7, 6, -5, 4];
-    bubble_sort(&mut arr);
-}
+fn main() {}
 
 fn bubble_sort(arr: &mut [isize]) -> Vec<isize> {
     for i in 0..arr.len() {
@@ -16,6 +13,26 @@ fn bubble_sort(arr: &mut [isize]) -> Vec<isize> {
     arr.to_owned()
 }
 
+fn optimized_bubble_sort(arr: &mut [isize]) -> Vec<isize> {
+    for i in 0..arr.len() {
+        // Check if swapping occurs
+        let mut swapped = false;
+        for j in 0..arr.len() - i - 1 {
+            if arr[j] > arr[j + 1] {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        // No swapping means elements are already sorted
+        if !swapped {
+            break;
+        }
+    }
+    arr.to_owned()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,6 +41,14 @@ mod tests {
     fn test_bubble_sort() {
         let mut arr = vec![8, 1, -8, 5, 0, 7, -2, 2, -6, -1, 3, -3, -4, -7, 6, -5, 4];
         let sorted_arr = bubble_sort(&mut arr);
+        arr.sort();
+        assert_eq!(arr, sorted_arr);
+    }
+
+    #[test]
+    fn test_optimized_bubble_sort() {
+        let mut arr = vec![8, 1, -8, 5, 0, 7, -2, 2, -6, -1, 3, -3, -4, -7, 6, -5, 4];
+        let sorted_arr = optimized_bubble_sort(&mut arr);
         arr.sort();
         assert_eq!(arr, sorted_arr);
     }
