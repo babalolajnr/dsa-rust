24c7195f95e7701e9986ac86ee15dcfb85655ec6
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-08 07:24:50 +0100

    add test: test_bubble_sort

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut arr: Vec<i32> = vec![8, 1, 5, 7, 2, 3, 6, 4];
+        let mut arr = vec![8, 1, -8, 5, 0, 7, -2, 2, -6, -1, 3, -3, -4, -7, 6, -5, 4];
     bubble_sort(&mut arr);
 }
 
-fn bubble_sort(arr: &mut [i32]) {
+fn bubble_sort(arr: &mut [isize]) -> Vec<isize> {
     for i in 0..arr.len() {
         for j in 0..arr.len() - i - 1 {
             if arr[j] > arr[j + 1] {
@@ -13,5 +13,18 @@ fn bubble_sort(arr: &mut [i32]) {
             }
         }
     }
-    println!("{:?}", arr);
+    arr.to_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bubble_sort() {
+        let mut arr = vec![8, 1, -8, 5, 0, 7, -2, 2, -6, -1, 3, -3, -4, -7, 6, -5, 4];
+        let sorted_arr = bubble_sort(&mut arr);
+        arr.sort();
+        assert_eq!(arr, sorted_arr);
+    }
 }
